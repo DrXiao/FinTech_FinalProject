4d2fa249f46c1487dda83f271a9cd68a3648f5ef
ifTNT <iftnt1999@gmail.com>
2021-01-11 22:52:35 +0800

    Half completed 0-1knapsack

--- src/knapsack.py
+++ src/knapsack.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import numpy as np
 from common_dataset import StockDataset
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
 
 # Calculate the total and yearly mean return every year
 # by the Lump-Sum method
@@ -38,8 +41,29 @@ def return_by_dca(date, price):
 
 # Select best portfolio using risk and potential return via dynamic programming
 def select_portfolio(train_d, excepted_risk=0.1):
+  stocks = np.array(train_d['簡稱'].unique())
+  dp_item = []
+  for stock in stocks:
+    # Estimate the return
+    stock_d = train_d[train_d['簡稱']==stock]
+    train_x = stock_d.drop
+    excepted_fields = ['證券代碼', '簡稱', '市值(百萬元)']
+    self.train_d = dataset.drop(excepted_fields, axis=1).reindex()
+    train_y = train_y[train_d['簡稱']]
+    clf = SVR(kernel='rbf', gamma=0.1)
+    clf.fit(, y)
+
+    # Calculate the risk
+    price = train_d[train_d['簡稱']==stock]['收盤價(元)_年']
+    mean_price = np.repeat(np.mean(price), len(price))
+    rmse = mean_squared_error(price, mean_price, squared = False)
+    dp_item.append({stock:stock, risk:rmse})
+    print(f"Risk of {stock}: {rmse}")
+
+  # Select the portfolio via dynamic programming
+
+
   return ['台積電', '聯電', '聯發科', '鴻海']
-  pass
 
 def evaluate_portfolio(portfolio, val_d):
   avg_lumpsum_return = 0
@@ -91,6 +115,10 @@ if __name__=="__main__":
   # Perform temperal validation
   date_list = dataset.filted_d['年月'].unique()
   training_mask = pd.Series(np.zeros((len(dataset.filted_d)), dtype=bool))
+
+  total_lumpsum_return = []
+  total_dca_return = []
+
   for tv,date in enumerate(date_list[:-1]):
     print(f"TV={tv}, Split={date}")
     
@@ -119,4 +147,27 @@ if __name__=="__main__":
     print(f"\tTotal Return: {avg_dca_return:.2f}%")
     print(f"\tYearly Mean Return: {avg_dca_yearly_mean_return:.2f}%")
 
+    # Accumalate total yearly mean return of both Lump-Sum and DCA
+    total_lumpsum_return.append(avg_lumpsum_yearly_mean_return)
+    total_dca_return.append(avg_dca_yearly_mean_return)
+
     print("---")
+
+  # Convert the totally return
+  # Exclude the last TV
+  total_lumpsum_return  = np.array(total_lumpsum_return[:-1])
+  total_dca_return = np.array(total_dca_return[:-1])
+
+  x_tv = list(range(len(date_list)-2))
+
+  width = 0.25
+  plt.bar(x_tv, total_lumpsum_return, label='Lump-Sum', width=0.25)
+  plt.bar([p + width for p in x_tv], total_dca_return, label='DCA', width=0.25)
+  plt.xticks([p + width/2 for p in x_tv], x_tv)
+  plt.yticks(np.arange(0, 200, 1))
+  plt.yscale('log')
+  plt.legend()
+  plt.title('Returns of 0/1 Knapsack')
+  plt.xlabel('Epochs of Temporal Validation')
+  plt.ylabel('Yearly Mean Return(%)')  
+  plt.show()
