f9ef1a0c5a2d6890e9524f9c9f1aa25d0827749c
Dr.Xiao <zxc71230@gmail.com>
2021-01-12 19:31:00 +0800

    Reduce repeated code

--- src/KNN.py
+++ src/KNN.py
@@ -32,7 +32,6 @@ def dataset_preprocessing(dataset_train):
 
 
 if __name__ == "__main__":
-
     dataset_train = pd.read_csv(data_path / dataset_file)
     dataset_train.index += 1
 
@@ -50,81 +49,22 @@ if __name__ == "__main__":
 
     train_data, test_data, train_label, test_label = \
         train_test_split(dataset_trainable, dataset_label,
-                         test_size=0.5, shuffle=False)
+                         test_size=1, shuffle=False)
     train_label = train_label.astype('int')
     test_label = test_label.astype('int')
 
-    knn_obj = KNeighborsClassifier(5)
+    knn_obj = KNeighborsClassifier()
 
     knn_obj.fit(train_data, train_label)
 
     threshold = train_data['收盤價(元)_年'].sum() / len(train_data)
     # print(dataset_info)
-    select_mode = int(input('1. Train Test,\n2. Formal test\t: '))
+    select_mode = 2 # int(input('1. Train Test,\n2. Formal test\t: '))
     if select_mode == 1:
         test_pred = knn_obj.predict(test_data)
         test_info_data = pd.merge(
             dataset_info, test_data, left_index=True, right_index=True)
         test_info_data['ReturnMean_year_Label'] = test_pred
-
-        stocks_dict = {}
-        profits = {}
-        years = []
-
-        start_year = list(test_info_data.head(1)["年月"])[0] // 100
-        years_len = 1
-        years.append(start_year)
-        for _, row in test_info_data.iterrows():
-            if(row["年月"] // 100 > start_year):
-                start_year = row["年月"] // 100
-                years.append(start_year)
-                years_len += 1
-            if row["ReturnMean_year_Label"] == -1:
-                if row["簡稱"] in stocks_dict.keys() and len(stocks_dict[row["簡稱"]]) < 2 and (stocks_dict[row["簡稱"]][0] - row["收盤價(元)_年"]) / stocks_dict[row["簡稱"]][0] > 0.05:
-                    stocks_dict[row["簡稱"]].append(row["收盤價(元)_年"])
-                    profits[row["簡稱"]].append(row["收盤價(元)_年"])
-            if row["收盤價(元)_年"] - threshold * 1.2:
-                if row["簡稱"] in stocks_dict.keys():
-                    if len(stocks_dict[row["簡稱"]]) < 2 and (row["收盤價(元)_年"] - stocks_dict[row["簡稱"]][0]) / stocks_dict[row["簡稱"]][0] > 0.10:  # 賣
-                        stocks_dict[row["簡稱"]].append(row["收盤價(元)_年"])
-                        profits[row["簡稱"]].append(row["收盤價(元)_年"])
-                elif len(stocks_dict) < 4:
-                    stocks_dict[row["簡稱"]] = [row["收盤價(元)_年"]]   # 買
-                    profits[row["簡稱"]] = [row["收盤價(元)_年"]]
-            if row["簡稱"] in stocks_dict.keys() and len(profits[row["簡稱"]]) < years_len:
-                if len(stocks_dict[row["簡稱"]]) < 2:
-                    profits[row["簡稱"]].append(row["收盤價(元)_年"])
-                else:
-                    profits[row["簡稱"]].append(profits[row["簡稱"]][-1])
-
-        print(years)
-        print(stocks_dict)
-        print(profits)
-        max_length = 0
-        for key in profits:
-            if len(profits[key]) > max_length:
-                max_length = len(profits[key])
-        for key in profits:
-            if len(profits[key]) < max_length:
-                profits[key].append(profits[key][-1])
-
-        plt.subplots()
-        dict_iter = iter(stocks_dict)
-        for i in range(len(stocks_dict.keys())):
-            plt.subplot(len(stocks_dict.keys()), 1, i + 1)
-            plt.xlabel("year")
-            plt.ylabel("price")
-            plt.plot(years, profits[next(dict_iter)])
-        plt.show()
-        print('----------------------')
-
-        for key in stocks_dict:
-            if len(stocks_dict[key]) == 2:
-                print(
-                    key, ":", 100*(stocks_dict[key][1] - stocks_dict[key][0]) / stocks_dict[key][0], "%")
-            else:
-                print(
-                    key, ":", 100*(profits[key][-1] - profits[key][0]) / profits[key][0], "%")
 
     elif select_mode == 2:
         dataset_test = pd.read_csv(data_path / dataset_test_file)
@@ -137,62 +77,69 @@ if __name__ == "__main__":
         test_info_data = pd.merge(
             dataset_test_info, dataset_test_trainable, left_index=True, right_index=True)
         test_info_data['ReturnMean_year_Label'] = test_pred
-        stocks_dict = {}
-        profits = {}
-        years = []
 
-        print(test_info_data)
-        start_year = list(test_info_data.head(1)["年月"])[0] // 100
-        years_len = 1
-        years.append(start_year)
-        for _, row in test_info_data.iterrows():
-            if(row["年月"] // 100 > start_year):
-                start_year = row["年月"] // 100
-                years.append(start_year)
-                years_len += 1
-            if row["ReturnMean_year_Label"] == -1:
-                if row["簡稱"] in stocks_dict.keys() and len(stocks_dict[row["簡稱"]]) < 2 and (stocks_dict[row["簡稱"]][0] - row["收盤價(元)_年"]) / stocks_dict[row["簡稱"]][0] > 0.05:
+    correct = 0
+
+    for idx in range(len(test_pred)):
+        if test_pred[idx] == dataset_test_label[idx]:
+            correct += 1
+    print('Accurancy :', correct / len(test_pred))
+
+    stocks_dict = {}
+    profits = {}
+    years = []
+
+    start_year = list(test_info_data.head(1)["年月"])[0] // 100
+    years_len = 1
+    years.append(start_year)
+    for _, row in test_info_data.iterrows():
+        if(row["年月"] // 100 > start_year):
+            start_year = row["年月"] // 100
+            years.append(start_year)
+            years_len += 1
+        if row["ReturnMean_year_Label"] == -1:
+            if row["簡稱"] in stocks_dict.keys() and len(stocks_dict[row["簡稱"]]) < 2 and (stocks_dict[row["簡稱"]][0] - row["收盤價(元)_年"]) / stocks_dict[row["簡稱"]][0] > 0.05:
+                stocks_dict[row["簡稱"]].append(row["收盤價(元)_年"])
+                profits[row["簡稱"]].append(row["收盤價(元)_年"])
+        if row["收盤價(元)_年"] - threshold * 1.2:
+            if row["簡稱"] in stocks_dict.keys():
+                if len(stocks_dict[row["簡稱"]]) < 2 and (row["收盤價(元)_年"] - stocks_dict[row["簡稱"]][0]) / stocks_dict[row["簡稱"]][0] > 0.10:  # 賣
                     stocks_dict[row["簡稱"]].append(row["收盤價(元)_年"])
                     profits[row["簡稱"]].append(row["收盤價(元)_年"])
-            if row["收盤價(元)_年"] - threshold * 1.2:
-                if row["簡稱"] in stocks_dict.keys():
-                    if len(stocks_dict[row["簡稱"]]) < 2 and (row["收盤價(元)_年"] - stocks_dict[row["簡稱"]][0]) / stocks_dict[row["簡稱"]][0] > 0.10:  # 賣
-                        stocks_dict[row["簡稱"]].append(row["收盤價(元)_年"])
-                        profits[row["簡稱"]].append(row["收盤價(元)_年"])
-                elif len(stocks_dict) < 4:
-                    stocks_dict[row["簡稱"]] = [row["收盤價(元)_年"]]   # 買
-                    profits[row["簡稱"]] = [row["收盤價(元)_年"]]
-            if row["簡稱"] in stocks_dict.keys() and len(profits[row["簡稱"]]) < years_len:
-                if len(stocks_dict[row["簡稱"]]) < 2:
-                    profits[row["簡稱"]].append(row["收盤價(元)_年"])
-                else:
-                    profits[row["簡稱"]].append(profits[row["簡稱"]][-1])
-
-        print(years)
-        print(stocks_dict)
-        print(profits)
-        max_length = 0
-        for key in profits:
-            if len(profits[key]) > max_length:
-                max_length = len(profits[key])
-        for key in profits:
-            if len(profits[key]) < max_length:
-                profits[key].append(profits[key][-1])
-
-        plt.subplots()
-        dict_iter = iter(stocks_dict)
-        for i in range(len(stocks_dict.keys())):
-            plt.subplot(len(stocks_dict.keys()), 1, i + 1)
-            plt.xlabel("year")
-            plt.ylabel("price")
-            plt.plot(years, profits[next(dict_iter)])
-        plt.show()
-        print('----------------------')
-
-        for key in stocks_dict:
-            if len(stocks_dict[key]) == 2:
-                print(
-                    key, ":", 100*(stocks_dict[key][1] - stocks_dict[key][0]) / stocks_dict[key][0], "%")
+            elif len(stocks_dict) < 4:
+                stocks_dict[row["簡稱"]] = [row["收盤價(元)_年"]]   # 買
+                profits[row["簡稱"]] = [row["收盤價(元)_年"]]
+        if row["簡稱"] in stocks_dict.keys() and len(profits[row["簡稱"]]) < years_len:
+            if len(stocks_dict[row["簡稱"]]) < 2:
+                profits[row["簡稱"]].append(row["收盤價(元)_年"])
             else:
-                print(
-                    key, ":", 100*(profits[key][-1] - profits[key][0]) / profits[key][0], "%")
+                profits[row["簡稱"]].append(profits[row["簡稱"]][-1])
+
+    print(years)
+    print(stocks_dict)
+    print(profits)
+    max_length = 0
+    for key in profits:
+        if len(profits[key]) > max_length:
+            max_length = len(profits[key])
+    for key in profits:
+        if len(profits[key]) < max_length:
+            profits[key].append(profits[key][-1])
+
+    plt.subplots()
+    dict_iter = iter(stocks_dict)
+    for i in range(len(stocks_dict.keys())):
+        plt.subplot(len(stocks_dict.keys()), 1, i + 1)
+        plt.xlabel("year")
+        plt.ylabel("price")
+        plt.plot(years, profits[next(dict_iter)])
+    plt.show()
+    print('----------------------')
+
+    for key in stocks_dict:
+        if len(stocks_dict[key]) == 2:
+            print(
+                key, ":", 100*(stocks_dict[key][1] - stocks_dict[key][0]) / stocks_dict[key][0], "%")
+        else:
+            print(
+                key, ":", 100*(profits[key][-1] - profits[key][0]) / profits[key][0], "%")    
